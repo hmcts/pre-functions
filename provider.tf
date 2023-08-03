c5411a348271e2749992d6b7bbc5deac91ae68e1
Zoe Cope <zoe.cope@hmcts.net>
2023-08-03 13:58:25 +0100

    add provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.65.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {
+  }
+}
