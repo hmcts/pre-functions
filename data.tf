9877f76bd15daec5a6e6288d473a5c886d2c35a8
Zoe Cope <zoe.cope@hmcts.net>
2023-08-03 13:42:47 +0100

    add more data resources

--- data.tf
+++ data.tf
@@ -6,3 +6,15 @@ data "azurerm_key_vault" "keyvault" {
   name                = var.env == "prod" ? "${var.product}-hmctskv-${var.env}" : "${var.product}-${var.env}"
   resource_group_name = data.azurerm_resource_group.rg.name
 }
+
+data "azurerm_subscription" "current" {}
+
+data "azurerm_storage_account" "final_sa" {
+  name                = "${var.product}finalsa${var.env}"
+  resource_group_name = "${var.product}-${var.env}"
+}
+
+data "azurerm_storage_account" "ingest_sa" {
+  name                = "${var.product}ingestsa${var.env}"
+  resource_group_name = "${var.product}-${var.env}"
+}
